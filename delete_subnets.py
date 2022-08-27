9d37b793884ac2b8e6f7db9a8be2326f9cb87301
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-08-27 16:27:37 +0530

    Update in code

--- delete_subnets.py
+++ delete_subnets.py
@@ -5,31 +5,30 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 
-AWS_REGION = input("Please enter the AWS_REGION")
+REGION = input("Please enter the AWS REGION")
 
-# this is the configration for the logger
+# this is the configration for the logger_for
 
-logger = logging.getLogger()
+logger_for = logging.getlogger_for()
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s: %(levelname)s: %(message)s')
 
-vpc_client = boto3.client("ec2", region_name=AWS_REGION)
+response = boto3.client("ec2", region_name=REGION)
 
 
 def del_subnet(subnet_id):
   
     try:
-        response = vpc_client.del_subnet(SubnetId=subnet_id)
+        res = response.del_subnet(SubnetId=subnet_id)
 
     except ClientError:
-        logger.exception('Could not delete the subnet.')
+        logger_for.exception('Oops sorry, Your subnet can not be deleted.')
         raise
     else:
-        return response
+        return res
 
 
 if __name__ == '__main__':
-    # SUBNET_ID = 'subnet-0d13cb95ea66f68e1'
-    SUBNET_ID = input("Enter the subnet id")
-    subnet = del_subnet(SUBNET_ID)
-    logger.info(f'Your Subnet {SUBNET_ID} is deleted successfully.')
+    ID = input("Enter the subnet id")
+    subnet = del_subnet(ID)
+    logger_for.info(f'Your Subnet {ID} is deleted successfully.')
